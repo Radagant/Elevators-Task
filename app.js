a35465db20996310d7a68c98da638b104a7fdc01
Radagant <radagant@gmail.com>
2022-03-04 19:50:31 +0300

    Лифт теперь двигается с нужной скоростью. Добавил функцию отдыха лифта, индикацию кнопки при движении лифта к этажу, стрелочное направление движения лифта и анимацию мерцания лифта вне состояния отдыха.

--- app.js
+++ app.js
@@ -1,33 +1,76 @@
 const App = {
-    data(){
-        return {
-            targetFloor: 1,
-            currentFloor: 1,
-            currentFloorPx: 0,
-            elevatorIsMoving: false,
-        }
-    },
-
-    methods: {
-        floorClickHandler (event) {
-            this.targetFloor = (event.target.innerText)
-            elevator = this.$refs.elevator
-            if (!this.elevatorIsMoving) {
-                this.elevatorIsMoving = true
-                elevator.style.marginBottom = `${this.currentFloorPx + ((this.targetFloor-this.currentFloor)*90)}px`
-                // elevator.classList.add(`moving-towards-${this.targetFloor}`)
-                // elevator.classList.remove(`moving-towards-${this.targetFloor}`)
-                this.currentFloorPx += (this.targetFloor-this.currentFloor)*90
-                this.currentFloor = this.targetFloor
-                this.elevatorIsMoving = false
-
-            }
-
-
-        }
+  data(){
+    return {
+      targetFloor: 1,
+      currentFloor: 1,
+      currentFloorPx: 0,
+      elevatorIsMoving: false,
+      elevatorDirectionMarker: 0,
+      elevatorDirection: ''
     }
+  },
+
+  methods: {
+    floorClickHandler (event) {
+      if (!this.elevatorIsMoving) {
+        this.elevatorMovingHandler(event)
+      }
+      else {
+
+      }
+    },
+    
+    async elevatorMovingHandler (event) {
+      delay = (ms) => {
+        return new Promise (resolve => setTimeout(() => resolve(), ms))
+      }
+      this.targetFloor = (event.target.innerText)
+      if (this.targetFloor-this.currentFloor > 0) {
+        elevatorDirectionMarker = 1
+        this.elevatorDirection = '↑'
+      } else { 
+        elevatorDirectionMarker = -1
+        this.elevatorDirection = '↓'
+      }
+      elevator = this.$refs.elevator
+      this.elevatorIsMoving = true
+      event.target.style.backgroundColor = 'rgb(112, 226, 92)'
+      //Мерцание лифта в движении
+      elevator.style.backgroundColor = 'rgb(199, 199, 233)'
+      let flickering1 = setInterval(() => elevator.style.backgroundColor = '', 400)
+      let flickering2 = setInterval(() => elevator.style.backgroundColor = 'rgb(199, 199, 233)', 800)
+      
+        
+    
+      //Первое передвижение на этаж
+      elevator.style.marginBottom = `${this.currentFloorPx + (90 * elevatorDirectionMarker)}px`
+      this.currentFloorPx += 90 * elevatorDirectionMarker
+        //Оставшиеся передвижения по этажам
+        for (let i = 1; i < Math.abs(this.targetFloor-this.currentFloor); i++) {
+          // elevator.style.marginBottom = `${this.currentFloorPx + (90 * elevatorDirectionMarker)}px`
+          // this.currentFloorPx += 90 * elevatorDirectionMarker
+          await delay(1000).then(() => {
+            elevator.style.marginBottom = `${this.currentFloorPx + (90 * elevatorDirectionMarker)}px`
+            this.currentFloorPx += 90 * elevatorDirectionMarker
+          })
+        }
+
+        //Задержка, пока лифт перейдёт с предпоследнего на первый этаж (костыль)
+        await delay(1000)
+        event.target.style.backgroundColor = ''
+        this.elevatorDirection = ''
+        //Лифт в состоянии отдыха
+        await delay (3000)
+        //Остановка мерцания
+        clearTimeout(flickering1)
+        clearTimeout(flickering2)
+        elevator.style.backgroundColor = ''
+
+        this.currentFloor = this.targetFloor
+        this.elevatorIsMoving = false
+    }
+  }
 }
 
 app = Vue.createApp(App)
-
 app.mount('#app')
